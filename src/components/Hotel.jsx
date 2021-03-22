b99f35d89903f1b5a8f23bf4cf8faab3205f24f5
borsikapiros <73432795+borsikapiros@users.noreply.github.com>
2021-03-22 15:49:02 +0100

    final

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function Hotel (props) {
-    
+export default function Hotel(props) {
     const [clicked, setClicked] = useState(false);
 
     if (props.data !== "") {
@@ -10,11 +9,21 @@ export default function Hotel (props) {
                 {props.data.map((hotel) => (
                     <div className="hotel-container" key={hotel.id}>
                         <h2>{hotel.name}</h2>
-                        <div className="hotel-details">{clicked === hotel.id ? `${hotel.city} (${hotel.stars})` : ""}</div>
-                        <button onClick={()=>setClicked(clicked === false? hotel.id : false)}>{ clicked ? "Show less" : "Show more"}</button>
+                        <div className="hotel-details">
+                            {clicked === hotel.id
+                                ? `${hotel.city}, ${hotel.stars} stars`
+                                : ""}
+                        </div>
+                        <button
+                            onClick={() =>
+                                setClicked(clicked === false ? hotel.id : false)
+                            }
+                        >
+                            {clicked ? "Show less" : "Show more"}
+                        </button>
                     </div>
                 ))}
             </div>
-        )
+        );
     }
 }
